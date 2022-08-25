372122689d068b09795526f7f9c55694c23b1296
Siarheimasiuk <winst73@gmail.com>
2022-08-26 00:51:38 +0300

    Add CustomViewController.

--- IOS7-HM13-SettingsScreen/Sources/ViewController.swift
+++ IOS7-HM13-SettingsScreen/Sources/ViewController.swift
@@ -10,62 +10,15 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    private var setups: [[Setups]]?
-    
-//     var namesSetup = ["Авиарежим",
-//                       "Wi-Fi",
-//                       "Bluetooth",
-//                       "Сотовая связь",
-//                       "Режим модема",
-//                       "VPN",
-//                       "Уведомления",
-//                       "Звуки, тактильные сигналы",
-//                       "Не беспокоить",
-//                       "Экранное время",
-//                       "Основные",
-//                       "Пункты управления",
-//                       "Экран и яркость",
-//                       "Экран <<Домой>>",
-//                       "Универсальный доступ",
-//                       "Обои",
-//                       "Siri и Поиск",
-//                       "Face ID и код-пароль",
-//                       "Экстренный вызов - SOS",
-//                       "Уведомление о контакте",
-//                       "Аккумулятор",
-//                       "Конфиденциальность"]
-//
-//    var images = ["airplane",
-//                  "wifi",
-//                  "wifi",
-//                  "antenna.radiowaves.left.and.right",
-//                  "personalhotspot",
-//                  "wifi",
-//                  "bell.badge.fill",
-//                  "speaker.wave.3.fill",
-//                  "moon.fill",
-//                  "hourglass",
-//                  "gear",
-//                  "switch.2",
-//                  "textformat.size",
-//                  "wifi",
-//                  "moon.fill",
-//                  "wifi",
-//                  "moon.fill",
-//                  "faceid",
-//                  "moon.fill",
-//                  "wifi",
-//                  "battery.100",
-//                  "hand.raised.fill",]
-//
-//    var colors = ["", ""]
-
+     var setups: [[Setups]]?
+ 
     // MARK: - Outlets
 
     private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CustomViewController.self, forCellReuseIdentifier: "CustomViewController")
         tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     } ()
 
@@ -73,9 +26,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setups = Setups.setups
         view.backgroundColor = .white
         title = "Настройки"
-        //navigationController?.navigationBar.prefersLargeTitles = true
         setupHierarchy()
         setupLayout()
     }
@@ -89,8 +42,6 @@ class ViewController: UIViewController {
     private func setupLayout() {
         tableView.snp.makeConstraints {
             $0.top.right.bottom.left.equalTo(view)
-//            $0.trailing.bottom.leading.equalToSuperview()
-//            $0.top.equalTo(view.safeAreaLayoutGuide).offset(0)
         }
     }
 }
