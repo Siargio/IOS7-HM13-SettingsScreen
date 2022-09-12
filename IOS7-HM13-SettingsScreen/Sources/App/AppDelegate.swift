46b2924a72c5eadbfa69be4d9136fe9ac124e021
Siarheimasiuk <winst73@gmail.com>
2022-09-12 15:46:10 +0300

    MVC.

---
+++ IOS7-HM13-SettingsScreen/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  IOS7-HM13-SettingsScreen
+//
+//  Created by Sergio on 25.08.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ IOS7-HM13-SettingsScreen/Sources/Scene/View/TableViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionsViewController.swift
+//  IOS7-HM13-SettingsScreen
+//
+//  Created by Sergio on 25.08.22.
+//
+
+import UIKit
+
